fc6e1b87055549ffa96196d35b0de59ff435af8e
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-16 13:08:01 -0500

    Agrega archivos de configuración y estructura inicial del proyecto
    - Crea el archivo .gitignore para excluir archivos generados por IDEs y otros archivos innecesarios.
    - Implementa la interfaz Cola y la clase NodoCola para la estructura de datos de cola.
    - Añade la clase ColaVacia para manejar excepciones relacionadas con la cola vacía.
    - Incluye archivos de configuración XML para la codificación y la estructura del proyecto.

---
+++ src/tadCola/Cola.java
@@ -0,0 +1,16 @@
+﻿package tadCola;
+
+public interface Cola <T>{
+
+    void encolar(T dato);
+    T desencolar();
+    void decapitar();
+    void imprimirCola();
+    int numElemCola();
+    void invertirCola();
+    String getNombre();
+    void mostrarEstadoCola();
+    void eliminarCola();
+    boolean colaVacia();
+
+}
