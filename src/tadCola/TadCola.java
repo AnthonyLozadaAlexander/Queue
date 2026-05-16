16e6360c1af1b89d5250989a0ac58bd06c6a2edc
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-16 13:08:15 -0500

    Agrega archivos de configuración y estructura inicial del proyecto

---
+++ src/tadCola/TadCola.java
@@ -0,0 +1,38 @@
+﻿package tadCola;
+
+public class TadCola<T> implements Cola<T> {
+    @Override
+    public void encolar(T dato) {
+
+    }
+
+    @Override
+    public T desencolar() {
+        return null;
+    }
+
+    @Override
+    public void decapitar() {
+
+    }
+
+    @Override
+    public void imprimirCola() {
+
+    }
+
+    @Override
+    public int numElemCola() {
+        return 0;
+    }
+
+    @Override
+    public void invertirCola() {
+
+    }
+
+    @Override
+    public String getNombre() {
+        return "";
+    }
+}
