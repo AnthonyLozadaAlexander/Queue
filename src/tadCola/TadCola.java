d2d08d3ebef1f2b5acbaa130e0ea3642969d8236
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-16 13:15:10 -0500

    Agrega constructores para inicializar la cola
    Se añaden dos constructores a la clase TadCola: uno por defecto que inicializa la cola vacía y otro que permite establecer un nombre para la cola. Esto mejora la flexibilidad en la creación de instancias de la cola.

--- src/tadCola/TadCola.java
+++ src/tadCola/TadCola.java
@@ -1,6 +1,22 @@
 ﻿package tadCola;
 
 public class TadCola<T> implements Cola<T> {
+
+    private NodoCola<T> principio;
+    private NodoCola<T> fin;
+    private String nombre;
+
+    public TadCola(){
+        principio = null;
+        fin = null;
+    }
+
+    public TadCola(String nombre){
+        super();
+        principio = null;
+        fin = null;
+        this.nombre = nombre;
+    }
     @Override
     public void encolar(T dato) {
 
